f806d7e81dd5391c78522da16911e68f332936a0
Maksim Kolotev <maxrocks@my.yorku.ca>
2023-11-22 11:28:05 -0500

    finished product

--- Frontend/src/pages/CreateBook.jsx
+++ Frontend/src/pages/CreateBook.jsx
@@ -3,12 +3,14 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const CreateBook = () =>{
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [publishYear, setPublishYear] = useState('');
     const [loading, setLoading] = useState(false);
+    const {enqueueSnackbar} = useSnackbar();
     const navigate = useNavigate();
 
     const handleSaveBook = () =>{
@@ -22,11 +24,13 @@ const CreateBook = () =>{
             .post('http://localhost:5555/books', data)
             .then(()=>{
                 setLoading(false);
+                enqueueSnackbar('Book created successfully!',{variant: 'success'});
                 navigate('/');
             })
             .catch((error)=>{
                 setLoading(false);
-                alert('An error occured');
+                //alert('An error occured');
+                enqueueSnackbar('Error', {variant: 'error'});
                 console.log(error)
             })
     }
